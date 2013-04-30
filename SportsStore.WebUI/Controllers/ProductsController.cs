e3c4b6bf985d9a915d4dc53ba9f133acea96a89c
Aneudy Guzman <aguzman@intellisys.com.do>
2013-04-30 16:06:46 -0400

    adding files

---
+++ SportsStore.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+    	private IProductsRepository productsRepository;
+		public ProductsController()
+		{
+			productsRepository = new FakeProductsRepository();
+		}
+        public ActionResult List()
+        {
+            return View(productsRepository.Products.ToList());
+        }
+
+    }
+}
