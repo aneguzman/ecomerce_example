6714feb9b4dec041db5d551963887430d091b5e7
Aneudy Guzman <aguzman@intellisys.com.do>
2013-05-01 15:01:29 -0400

    adding sqlProductRepository againg and adding git ignore

---
+++ SportsStore.Domain/Concrete/SqlProductsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete {
+	public class SqlProductsRepository: IProductsRepository
+	{
+		private Table<Product> produtcsTable;
+
+		public SqlProductsRepository(string connectionString)
+		{
+			produtcsTable = (new DataContext(connectionString)).GetTable<Product>();
+		}
+
+		public IQueryable<Product> Products
+		{
+			get { return produtcsTable; }
+		}
+	}
+}
